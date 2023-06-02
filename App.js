f5ebda9b0648072d0becdbc1ec741a5199c37909
Samyak Jain <jainsamyak2002.sj@gmail.com>
2023-06-02 22:22:58 +0530

    Text Input from user

--- App.js
+++ App.js
@@ -1,12 +1,37 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
 
 export default function App() {
+
+  const [inputMessage, setInputMessage] = useState('');
+
+  const handleButtonClick = () => {
+    console.log(inputMessage)
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
+      <View style={{ flex: 1, justifyContent: 'center' }}>
+        <Text>Results to be shown here!</Text>
+      </View>
+
+      <View style={{ flexDirection: 'row' }}>
+
+        <View style={{ flex: 1, marginLeft: 10, marginBottom: 20 }}>
+          <TextInput placeholder='Enter your Question' onChangeText={setInputMessage} />
+        </View>
+
+        <TouchableOpacity onPress={handleButtonClick}>
+          <View style={{ backgroundColor: 'red', padding: 5, marginRight: 10, marginBottom: 20 }}>
+            <Text>Send</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
+
       <StatusBar style="auto" />
-    </View>
+    </View >
   );
 }
 
