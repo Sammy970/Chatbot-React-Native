6eb4dc314a4acc0d9d1617e3bc24267126aa7fdf
Samyak Jain <jainsamyak2002.sj@gmail.com>
2023-06-03 08:58:19 +0530

    Added Generate Image feature and all api url and keys in key.json file

--- App.js
+++ App.js
@@ -2,22 +2,33 @@ import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
 import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
 
+import { OPENAI_CHAT, OPENAI_IMAGE, PAWAN_CHAT, PAWAN_IMAGE } from './keys.json';
+
 export default function App() {
+
+  const OPENAI_key = OPENAI_CHAT.api_key;
+  const pawanOpenAI_key = PAWAN_CHAT.api_key;
+
+  // Chat GPT
+  const OPENAI_url = OPENAI_CHAT.api_url;
+  const pawanOpenAI_url = PAWAN_CHAT.api_url;
+
+  // Dall E
+  const OPENAI_DALLE_URL = OPENAI_IMAGE.api_url;
+  const pawanOpenAI_DALLE_URL = PAWAN_IMAGE.api_url;
 
   const [inputMessage, setInputMessage] = useState('');
   const [outputMessage, setOutputMessage] = useState('Results to be shown here!');
 
   const handleButtonClick = () => {
 
-    const OPENAI_key = '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';
-
     console.log(inputMessage)
 
-    fetch('https://api.openai.com/v1/chat/completions', {
+    fetch(pawanOpenAI_url, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
-        'Authorization': `Bearer ${OPENAI_key}`
+        'Authorization': `Bearer ${pawanOpenAI_key}`
       },
       body: JSON.stringify({
         "messages": [{ "role": "user", "content": inputMessage }],
@@ -30,7 +41,27 @@ export default function App() {
 
   }
 
-  // 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
+  const generateImages = () => {
+
+    console.log(inputMessage)
+
+    fetch(pawanOpenAI_DALLE_URL, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${pawanOpenAI_key}`
+      },
+      body: JSON.stringify({
+        "prompt": inputMessage,
+        "n": 1,
+        "size": "1024x1024",
+      })
+    }).then((response) => response.json()).then((data) => {
+      console.log(data.data[0].url);
+      setOutputMessage(data.data[0].url);
+    })
+
+  }
 
   return (
     <View style={styles.container}>
